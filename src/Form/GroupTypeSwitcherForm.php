4bd213f3ec98265d3980e542cd055b52a11ccb79
Adriano Dias <adrianodias1994@gmail.com>
2019-09-09 11:42:03 +0200

    create some helper functions

--- src/Form/GroupTypeSwitcherForm.php
+++ src/Form/GroupTypeSwitcherForm.php
@@ -51,25 +51,48 @@ class GroupTypeSwitcherForm extends FormBase {
     ];
     return $form;
   }
-  
+
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
     // test case swich group (gid 1) from fut_open to fut_private
-
     $gid = 1;
 
-    $group = $this->switcherService->getGroups([1]);
+    // Selected group type in this case "private".
+    $selected_group_type = 'fut_private';
 
-    
+    $group = $this->switcherService->getGroups([1]);
     $my_group_gtype = $this->switcherService->getGroupTypes([$group->bundle()]);
 
+    $selected_group_gtype = $this->switcherService->getGroupTypes([$selected_group_type]);
 
-    $installed_plugins = $my_group_gtype->getInstalledContentPlugins();
 
-    var_dump($installed_plugins);
+    $myg_installed_plugins = $my_group_gtype->getInstalledContentPlugins();
+
+    $selected_installed_plugins = $selected_group_gtype->getInstalledContentPlugins();
+
+    // kint($selected_installed_plugins->getConfiguration());
+    echo "<h2>plugins</h2>";
+    var_dump(array_keys($myg_installed_plugins->getConfiguration()));
+    echo "<h2>group content types</h2>";
+    var_dump($this->switcherService->getGroupContentTypes());
+    // var_dump($this->switcherService->getGroupContentTypes(['fut_open-group_node-fut_post']));
+
+
+
+    // var_dump($selected_installed_plugins->getConfiguration() == $myg_installed_plugins->getConfiguration());
+    echo "<h2>group tables</h2>";
+    var_dump($this->switcherService->getEntityTables());
+    echo "<h2>group content tables</h2>";
+
+    var_dump($this->switcherService->getEntityTables('group_content'));
+    echo "<h2>group - fut_open- field tables</h2>";
+    var_dump($this->switcherService->getFieldTables('group','fut_open'));
+    echo "<h2>group_content - fut_open-group_node-fut_post - field tables</h2>";
+    var_dump($this->switcherService->getFieldTables('group_content','fut_open-group_node-fut_post'));
+
 
     exit();
 
--- src/GroupTypeSwitcherService.php
+++ src/GroupTypeSwitcherService.php
@@ -7,6 +7,7 @@ use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Component\Datetime\TimeInterface;
 use PDO;
 use Drupal\Core\Database\Query\SelectInterface;
+use Drupal\Core\Entity\EntityFieldManager;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 
@@ -44,6 +45,13 @@ class GroupTypeSwitcherService {
   protected $entityTypeManager;
 
   /**
+   * The entity field manager.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManager
+   */
+  protected $entityFieldManager;
+
+  /**
    * Constructs a new GroupTypeSwitcherService object.
    *
    * @param \Drupal\Core\Database\Driver\mysql\Connection $database
@@ -53,11 +61,12 @@ class GroupTypeSwitcherService {
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
    *   The logger channel factory.
    */
-  public function __construct(Connection $database, TimeInterface $date_time, LoggerChannelFactoryInterface $logger, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(Connection $database, TimeInterface $date_time, LoggerChannelFactoryInterface $logger, EntityTypeManagerInterface $entity_type_manager, EntityFieldManager $entity_field_manager) {
     $this->database = $database;
     $this->dateTime = $date_time;
     $this->logger = $logger->get('group_type_switcher');
     $this->entityTypeManager = $entity_type_manager;
+    $this->entityFieldManager = $entity_field_manager;
   }
 
   public function getGroups(array $ids = NULL) {
@@ -70,8 +79,62 @@ class GroupTypeSwitcherService {
     return (count($group_types) > 1) ? $group_types : reset($group_types);
   }
 
+  public function getGroupContentTypes(array $ids = NULL) {
+    $group_content_types = $this->entityTypeManager->getStorage('group_content_type')->loadMultiple($ids);
+    return (count($group_content_types) > 1) ? array_keys($group_content_types)  :array_keys($group_content_types);
+  }
+
+  public function getEntityTables($entity_type = 'group'){
+    $tables = [];
+
+    foreach (['base_table','data_table'] as $t) {
+      $tables[$t] = $this->entityTypeManager->getDefinition($entity_type)->get($t);
+    }
+    return $tables;
+  }
+
+  public function getFieldTables($entity_type_id = 'group', $bundle = 'fut_open'){
+
+    $fields = $this->entityFieldManager->getFieldDefinitions($entity_type_id, $bundle);
+    foreach ($fields as $field_name => $field_definition) {
+      if (!empty($field_definition->getTargetBundle())) {
+        $listFields[$field_name] = "{$entity_type_id}__{$field_name}";
+        // $listFields[$field_name]['label'] = $field_definition->getLabel();
+      }
+    }
+    // $rows = [];
+    // foreach ($listFields as $field_name => $info) {
+    //   $rows[$field_name] = $info;
+    // }
+    // return $rows;
+
+    return $listFields;
+  }
 
 
-  
+
+  public function updateBundleCols() {
+    if (!$activity_record->isNew()) {
+      $fields = [
+        'activity' => $activity_record->getActivityValue(),
+        'changed' => $this->dateTime->getRequestTime(),
+      ];
+      try {
+        $this->database->update('fut_activity')
+          ->fields($fields)
+          ->condition('activity_id', $activity_record->id())
+          ->execute();
+      }
+      catch (\Throwable $th) {
+        $this->logger->error($th->getMessage());
+        return FALSE;
+      }
+      return TRUE;
+    }
+  }
+
+
+
+
 
 }
